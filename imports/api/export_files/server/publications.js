689d672becaa86684e1557104a5b18c95d3ac574
Hugh Willson <hugh@octonary.com>
2016-07-09 05:39:15 -0400

    Make sure most recent export file is published.

--- imports/api/export_files/server/publications.js
+++ imports/api/export_files/server/publications.js
@@ -5,7 +5,7 @@ import exportFiles from '../collection.js';
 Meteor.publish('exportFiles.all', function publishExportFilesAll() {
   let cursor;
   if (this.userId) {
-    cursor = exportFiles.find();
+    cursor = exportFiles.find({}, { sort: { uploadedAt: -1 }, limit: 1 });
   } else {
     cursor = this.ready();
   }
